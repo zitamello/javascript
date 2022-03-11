909e840a94eca1e7e49f284b245af3d0de2971e3
Zita Mello <zitamello@gmail.com>
2022-03-11 14:15:31 -0300

    interpolation with strings

---
+++ variables.js
@@ -0,0 +1,12 @@
+let man = "James";
+let wife = "Mary";
+let son = "Eddy";
+
+let year_birth = "1989";
+console.log(man, "nasceu em", year_birth);
+console.log('Em 2000', man, 'casou-se com', wife);
+console.log(`Tiveram um filho chamado ${son}` + ' em 2005.');
+
+message = `The members of the family are ${man}, ${wife} and ${son}`;
+
+console.log(message);
