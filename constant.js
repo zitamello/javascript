397cad39f08a31fcaa0d9bc18fc07a981a79ac75
Zita Mello <zitamello@gmail.com>
2022-03-11 15:59:09 -0300

    constant.js added

---
+++ constant.js
@@ -0,0 +1,10 @@
+const nameClient = 'John Doe'
+let ageClient = 30;
+let peso = 80;
+const altura = 1.80;
+
+const imc = peso / (altura * altura);
+
+messageOutput = `O IMC do cliente ${nameClient} é ${imc}`;
+
+console.log(messageOutput);
