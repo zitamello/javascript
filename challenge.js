ddef2e7bd8c658583e76008bed0a17f7941e7b44
Zita Mello <zitamello@gmail.com>
2022-03-21 11:09:11 -0300

    new commit

--- challenge.js
+++ challenge.js
@@ -1,19 +1,23 @@
-let markHeight = 1.79;
-let markWeight = 87;
+const markHeight = 1.79;
+const markWeight = 87;
 
-let johnHeight = 1.95;
-let johnWeight = 92;
+const johnHeight = 1.95;
+const johnWeight = 92;
 
-markHeight = 1.88;
+
+/*markHeight = 1.88;
 markWeight = 95;
 
 johnHeight = 1.76;
 johnWeight = 85;
+*/
+
 
 const johnBMI = johnWeight / (johnHeight * johnHeight);
 const markBMI = markWeight / (markHeight ** 2);
 
 const markHigherThanJohn = markBMI > johnBMI;
+
 
 console.log(johnBMI, markBMI);
 console.log(markHigherThanJohn);
--- operator2.js
+++ operator2.js
@@ -17,21 +17,31 @@ console.log(!true && true);
 console.log(!true && false);
 */
 
-const usuario = 'Luis';
-const senha = '123456';
+// const usuario = 'Luis';
+// const senha = '123456';
 
-const login = usuario === 'Luis';
-const password = senha === '12345';
+// const login = usuario === 'Luis';
+// const password = senha === '12345';
 
-const loginSucessful = usuario === senha;
+// const loginSucessful = usuario === senha;
 
-console.log(`Usuario so pode logar se o login e o password for true. O login é true? ${loginSucessful}`);
+// console.log(`Usuario so pode logar se o login e o password for true. O login é true? ${loginSucessful}`);
 
-function speak() {
-    return "I'm speaking";
-}
+// function speak() {
+//     return "I'm speaking";
+// }
 
-let speaking = true;
+// let speaking = true;
 
-console.log(speaking && speak());
+// console.log(speaking && speak());
+
+
+const age = 18;
+if (age === 18) console.log("Congratulations, you've just became an adult!");
+
+console.log(18 === '18', "strict 18 === '18'");
+console.log(18 === 18, `strict 18 === 18 `);
+console.log(18 == '18', "loose 18 == '18'");
+
+const favorite = prompt("What's your favorite number?");
 
