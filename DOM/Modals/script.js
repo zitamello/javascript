c56239ed3126cc16a16cdd0e2e493050e5e25f6e
Zita Mello <zitamello@gmail.com>
2022-03-31 10:26:23 -0300

    working with modals

---
+++ DOM/Modals/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+
+//when several elements share the same class use querySelectorALL;
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for(let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);   
+} 
+
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
